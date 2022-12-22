8b02084650f2efd18ca916eff90adc4c7600fe8f
Marco <marcosuarezp95@gmail.com>
2022-12-22 19:15:34 -0400

    day 1 part two done

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,26 @@ fn main() {
     let value_split = value.split("\n");
     
     let mut elf_sum = 0;
-    let mut all_sums = Vec::<i32>::new();
+    let mut top_three: Vec<i32> = vec![0, 0, 0];
     for item in value_split.into_iter() {
         if item.trim().is_empty() {
-            all_sums.push(elf_sum);
+            for (idx, top_three_item) in top_three.to_owned().into_iter().enumerate() {
+                if &elf_sum > &top_three_item {
+                    if idx < (top_three.len() - 1) {
+                        top_three[idx + 1] = top_three[idx];
+                    }
+                    top_three[idx] = elf_sum;
+                    break;
+                }
+            }
             elf_sum = 0;
         } else {
             elf_sum += i32::from_str(item.trim()).expect("Error parsing number");
         }
     }
 
-    let max_sum = all_sums.into_iter().max();
-    match max_sum {
-        Some(value) => println!("Result is: {}", value),
-        None => println!("Error getting max value"),
-    }
+    let top_three_sum: i32 = top_three.iter().sum();
+    println!("Top three is: {:?}", top_three);
+    println!("Result is: {}", top_three_sum);
     
 }
