93dcf6036cd2f230ea856cb943890caddfbc3837
Marco <marcosuarezp95@gmail.com>
2022-12-25 12:35:26 -0400

    day 2 part two done

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
+use std::fmt;
 use std::{str::FromStr, collections::HashMap};
 mod input;
 
 #[derive(Hash, PartialEq, Eq, Clone, Copy, Debug)]
 enum ShapeName { ROCK, PAPER, SCISSORS, }
 #[derive(Hash, PartialEq, Eq)]
-enum ShapeNameShort { A, B, C, X, Y, Z, }
+enum ShapeNameShort { A, B, C }
+enum WantedOutcome { X, Y, Z, }
+
+impl fmt::Display for WantedOutcome {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            WantedOutcome::X => write!(f, "Loose"),
+            WantedOutcome::Y => write!(f, "Tie"),
+            WantedOutcome::Z => write!(f, "Win"),
+        }
+    }
+}
+
+impl FromStr for WantedOutcome {
+
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<WantedOutcome, Self::Err> {
+        match input {
+            "X" => Ok(WantedOutcome::X),
+            "Y" => Ok(WantedOutcome::Y),
+            "Z" => Ok(WantedOutcome::Z),
+            _   => Err(()),
+        }
+    }
+}
 
 impl FromStr for ShapeNameShort {
 
@@ -15,9 +41,6 @@ impl FromStr for ShapeNameShort {
             "A" => Ok(ShapeNameShort::A),
             "B" => Ok(ShapeNameShort::B),
             "C" => Ok(ShapeNameShort::C),
-            "X" => Ok(ShapeNameShort::X),
-            "Y" => Ok(ShapeNameShort::Y),
-            "Z" => Ok(ShapeNameShort::Z),
             _   => Err(()),
         }
     }
@@ -39,17 +62,24 @@ fn get_shape_name_by_short_name(ssn: &ShapeNameShort) -> ShapeName {
         (ShapeNameShort::A, ShapeName::ROCK),
         (ShapeNameShort::B, ShapeName::PAPER),
         (ShapeNameShort::C, ShapeName::SCISSORS),
-        (ShapeNameShort::X, ShapeName::ROCK),
-        (ShapeNameShort::Y, ShapeName::PAPER),
-        (ShapeNameShort::Z, ShapeName::SCISSORS),
     ])
         .get(ssn)
         .copied()
         .expect("Error getting shape name");
 }
 
-
 fn beats(s: &ShapeName) -> ShapeName {
+    return HashMap::from([
+        (ShapeName::ROCK, ShapeName::PAPER),
+        (ShapeName::PAPER, ShapeName::SCISSORS),
+        (ShapeName::SCISSORS, ShapeName::ROCK),
+    ])
+        .get(&s)
+        .copied()
+        .expect("Error getting beat shape");
+}
+
+fn loses_to(s: &ShapeName) -> ShapeName {
     return HashMap::from([
         (ShapeName::ROCK, ShapeName::SCISSORS),
         (ShapeName::PAPER, ShapeName::ROCK),
@@ -62,8 +92,8 @@ fn beats(s: &ShapeName) -> ShapeName {
 
 fn get_winner(s1: ShapeName, s2: ShapeName) -> Option<ShapeName> {
 
-    if beats(&s1) == s2 { return Some(s1); }
-    if beats(&s2) == s1 { return Some(s2); }
+    if loses_to(&s1) == s2 { return Some(s1); }
+    if loses_to(&s2) == s1 { return Some(s2); }
 
     return None;
 }
@@ -81,6 +111,14 @@ fn calculate_score(your_shape: ShapeName, winning_shape: Option<ShapeName>) -> u
 
 }
 
+fn get_wanted_shape(opponent_shape: &ShapeName, wanted_outcome: &WantedOutcome) -> ShapeName {
+    match wanted_outcome {
+        WantedOutcome::X => loses_to(&opponent_shape),
+        WantedOutcome::Y => *opponent_shape,
+        WantedOutcome::Z => beats(&opponent_shape),
+    }
+}
+
 fn main() {    
 
     let value = input::get_input().trim();
@@ -90,18 +128,18 @@ fn main() {
     for item in value_split.into_iter() {
         let item_split = item.trim().split(" ").collect::<Vec<&str>>();
 
-        let opponent_shape 
+        let opponent_shape: ShapeName 
             = get_shape_name_by_short_name(&ShapeNameShort::from_str(item_split[0])
               .expect("Error parsing shape string"));
-        let your_shape 
-            = get_shape_name_by_short_name(&ShapeNameShort::from_str(item_split[1])
-              .expect("Error parsing shape string"));
-        let score: u32 = calculate_score(your_shape, get_winner(opponent_shape, your_shape));
+        let wanted_outcome: WantedOutcome = WantedOutcome::from_str(item_split[1])
+            .expect("Error getting wanted outcome");
+        let wanted_shape: ShapeName = get_wanted_shape(&opponent_shape, &wanted_outcome);
+        let score: u32 = calculate_score(wanted_shape, get_winner(opponent_shape, wanted_shape));
 
         println!(
-            "Opponent: {:?} - You: {:?} - Score: {}", 
+            "Opponent: {:?} - You must: {} - Score: {}", 
             opponent_shape, 
-            your_shape, 
+            wanted_outcome, 
             score
         );
 
